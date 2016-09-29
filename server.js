dd02239add77910fa0cbb333dfa2a87fc3c79aae
avantikanimje <avantinimje@gmail.com>
2016-09-29 11:07:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ var articleOne = {
             </p>`
 };
 
+function createTemplate(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+
 var htmlTemplate = 
    ` 
     <html>
@@ -48,20 +54,17 @@ var htmlTemplate =
         </div> 
     </body>
     
-    
-    
-    
 </html>
 `;
-
-
+return htmlTemplate
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
