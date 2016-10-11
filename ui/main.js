8df3f007df34f2b32718e9c8495eda91e8d0e7af
avantikanimje <avantinimje@gmail.com>
2016-10-11 14:52:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img = document.getElementById('madi');
-img.onClick = function(){
-    img.style.marginLeft='100px';
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft  = marginLeft + 10;
+    img.style.marginLeft=marginLeft+'px';
 }
+img.onClick = function(){
+    var interval = setInterval(moveRight,100);
+};
