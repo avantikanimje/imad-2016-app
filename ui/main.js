69beb31e76d950bbcdc5a91eb6faf4c62a2c212a
avantikanimje <avantinimje@gmail.com>
2016-10-11 14:45:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//move the image
+var img = document.getElementById('madi');
+madi.onClick = function(){
+    madi.style.marginLeft='100px';
+}
